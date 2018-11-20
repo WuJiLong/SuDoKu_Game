9aee5c18c5aa69d45e12d51a57a3e1fa750d2d6b
奇隆 吳 <kftg3770@gmail.com>
2018-11-21 02:26:55 +0800

    設定介面外觀

--- app/src/main/java/com/example/wuqilong/sudoku_game/SuDoKu_setting_Activity.java
+++ app/src/main/java/com/example/wuqilong/sudoku_game/SuDoKu_setting_Activity.java
@@ -3,12 +3,11 @@ package com.example.wuqilong.sudoku_game;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SuDoKu_main_Activity extends AppCompatActivity {
+public class SuDoKu_setting_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sudoku_main);
+        setContentView(R.layout.activity_sudoku_setting);
     }
-    
 }
