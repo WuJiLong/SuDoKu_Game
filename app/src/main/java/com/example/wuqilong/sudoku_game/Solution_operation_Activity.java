6a61507c5bd4d0fea94f439888044ba4a1d5c997
奇隆 吳 <kftg3770@gmail.com>
2018-11-21 02:31:14 +0800

    數獨計算畫面外觀

---
+++ app/src/main/java/com/example/wuqilong/sudoku_game/Solution_operation_Activity.java
@@ -0,0 +1,13 @@
+package com.example.wuqilong.sudoku_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Solution_operation_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solution_operation);
+    }
+}
