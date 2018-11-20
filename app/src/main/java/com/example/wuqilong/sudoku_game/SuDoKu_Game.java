813d7fca7967062664593c2bcda7d5b03387fbb2
奇隆 吳 <kftg3770@gmail.com>
2018-11-21 02:00:27 +0800

    原始專案

---
+++ app/src/main/java/com/example/wuqilong/sudoku_game/SuDoKu_Game.java
@@ -0,0 +1,13 @@
+package com.example.wuqilong.sudoku_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuDoKu_Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_su_do_ku__game);
+    }
+}
